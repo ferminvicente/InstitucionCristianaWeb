9d17cc9b33b8a4763cae0266c6ef0449176d075c
eldar <eldar@DELLINSP153KFV>
2025-04-28 17:33:13 -0400

    Agregar archivos de proyecto.

---
+++ InstitucionCristianaWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace InstitucionCristianaWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
